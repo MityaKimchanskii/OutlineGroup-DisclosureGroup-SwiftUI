bc006244a6208457f51504b5cc8d123995581f69
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 22:19:56 -0700

    OutlineGroup, DisclosureGroup

--- OutlineGroup-SwiftUI/ContentView.swift
+++ OutlineGroup-SwiftUI/ContentView.swift
@@ -7,10 +7,77 @@
 
 import SwiftUI
 
+struct ListItem: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    var items: [ListItem]?
+}
+
+extension ListItem {
+    
+    static func all() -> [ListItem] {
+        
+        let animals = [ListItem(name: "🐫"), ListItem(name: "🐘"), ListItem(name: "🦣"), ListItem(name: "🦒"), ListItem(name: "🦌"), ListItem(name: "🦘")]
+        let fruits = [ListItem(name: "🍐"), ListItem(name: "🍋"), ListItem(name: "🍑"), ListItem(name: "🥬"), ListItem(name: "🥑"), ListItem(name: "🥥")]
+        let cars = [ListItem(name: "🚔"), ListItem(name: "🚍"), ListItem(name: "🚘"), ListItem(name: "🚎"), ListItem(name: "🚙"), ListItem(name: "🚕")]
+        
+        return [ListItem(name: "Animals", items: animals),
+                ListItem(name: "Fruits", items: fruits),
+                ListItem(name: "Cars", items: cars)
+        ]
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var sportsExpanded: Bool = true
+    let sports = ["🏂", "⛷", "🪂", "🏋🏻‍♀️", "🤼‍♀️", "🤸🏻‍♀️", "🏄🏼", "🏄🏾‍♂️"]
+    let fruits = ["🍏", "🍎", "🍊", "🍌", "🍉", "🍇", "🥝", "🍓"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            List {
+                
+                ForEach(ListItem.all()) { category in
+                    
+                    Section(header: EmptyView()) {
+                        
+                        OutlineGroup(category, children: \.items) { item in
+                            Text(item.name)
+                                .font(.custom("Arial", size: 20))
+                        }
+                    }
+                }
+            }
+            
+            Form {
+                
+                DisclosureGroup(isExpanded: $sportsExpanded) {
+                    HStack {
+                        ForEach(sports, id: \.self) { human in
+                            Text(human)
+                                .font(.system(size: 30))
+                        }
+                    }
+                } label: {
+                    Text("Sport")
+                }
+                
+                DisclosureGroup(isExpanded: $sportsExpanded) {
+                    HStack {
+                        ForEach(fruits, id: \.self) { fruit in
+                            Text(fruit)
+                                .font(.system(size: 30))
+                        }
+                    }
+                } label: {
+                    Text("Fruit")
+                }
+                
+            }
+        }
     }
 }
 

b49600da695aafde5dcaef77aafc866540eb665a
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 21:50:09 -0700

    Initial Commit

---
+++ OutlineGroup-SwiftUI/OutlineGroup_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineGroup_SwiftUIApp.swift
+//  OutlineGroup-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct OutlineGroup_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
